7a9306ab4756f47ef124b60eabee7fe4c7f8a179
Sagarsubedi619 <59858676+Sagarsubedi619@users.noreply.github.com>
2020-10-29 02:04:07 -0500

    Update pound.cpp

--- pound.cpp
+++ pound.cpp
@@ -7,12 +7,19 @@ Assignment: 5
 Date : 10/28/2020
 
 
-
-
+Description     : * This program demostrates the uses of class,constructor, and overloaded assignment operators 
+pre-condition   : * a default constructor, that sets the initial value to zero
+                  * a constructor that takes in a number (your choice of int, float, double, and so on) and sets the initial value to that
+                  * copy constructor
+                  * overloaded assignment operator
+                  * a way to set and get that value 
+                  * comparisons between different instances of your class
+                  * ways to input and output your unit
+                  * ways to get the name of the unit
+post-condition  :  output on the screen
 
 
 */
-
 
 
 
@@ -34,19 +41,19 @@ string result1;
 string result2;
 
 
-cout<<"This is the first object created "<<endl;
+cout<<"***This is the first object created*** "<<endl;
 cout<<""<<endl;
 result1=obj1.getinput(); 
 
 cout<<result1<<endl;
 
-cout<<"This is the second object created"<<endl;
+cout<<"***This is the second object created***"<<endl;
 cout<<""<<endl;
 result2=obj2.getinput();
  
 cout<<result1<<endl;
 
-cout<<"Adding(overloaded operator) two different pound value created from two different objects:"<<endl;
+cout<<"**Adding(overloaded operator) two different pound value created from two different objects:**"<<endl;
 cout<<endl;
 
 string objstr1=obj1.innum;
@@ -58,28 +65,28 @@ double num_double2 = std::stod(objstr2);
 double add = num_double1 +num_double2;
 cout<<add<<"  pound/s"<<endl;
 
-cout<<"subtract(overloaded operator) two different pound value created from two different objects:"<<endl;
+cout<<"**subtract(overloaded operator) two different pound value created from two different objects:**"<<endl;
 cout<<endl;
 double minus = num_double1 -num_double2;
 cout<<minus<<endl;
-cout<<"multiply(overloaded operator) two different pound value created from two different objects:"<<endl;
+cout<<"**multiply(overloaded operator) two different pound value created from two different objects:**"<<endl;
 cout<<endl;
 double mult = num_double1 *num_double2;
-cout<<mult<<endl;
-cout<<"divide(overloaded operator) two different pound value created from two different objects:"<<endl;
+cout<<mult<<"  pound/s"<<endl;
+cout<<"**divide(overloaded operator) two different pound value created from two different objects:**"<<endl;
 cout<<endl;
 double div = num_double1 /num_double2;
-cout<<div<<endl;
-cout<<"compare(overloaded operator) two different pound value created from two different objects:"<<endl;
+cout<<div<<"  pound/s"<<endl;
+cout<<"**compare(overloaded operator) two different pound value created from two different objects:**"<<endl;
 cout<<endl;
 if (num_double1 >num_double2){
- cout<<num_double1<< "  is greater than"<<num_double2<<endl; }
-else{cout<<num_double1<< "  is greater than"<<num_double2<<endl;}
+ cout<<num_double1<<"  pound/s"<< "  is greater than"<<num_double2<<endl; }
+else{cout<<num_double1<< "  is greater than"<<num_double2<<"  pound/s"<<endl;}
 
 cout<<endl;
 if (num_double1 ==num_double2){
- cout<<num_double1<< "  is equal"<<num_double2<<endl; }
-else{cout<<num_double1<< "  is not equal to"<<num_double2<<endl;}
+ cout<<num_double1<<"  pound/s"<< "  is equal"<<num_double2<<"  pound/s"<<endl; }
+else{cout<<num_double1<<"  pound/s"<< "  is not equal to"<<num_double2<<"  pound/s"<<endl;}
 
 
 
